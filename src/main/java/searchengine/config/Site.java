c38ff0fbce5b65d035ef102dc6bea6480e1ba3b9
Konstantin Shibkov <shibkov.k@gmail.com>
2022-10-06 23:10:23 +0300

    refactor

--- src/main/java/searchengine/config/Site.java
+++ src/main/java/searchengine/config/Site.java
@@ -1,13 +1,11 @@
 package searchengine.config;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-@RequiredArgsConstructor
 public class Site {
-    private final String url;
-    private final String name;
+    private String url;
+    private String name;
 }
--- src/main/java/searchengine/config/SitesList.java
+++ src/main/java/searchengine/config/SitesList.java
@@ -3,11 +3,13 @@ package searchengine.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "index-config")
 public class SitesList {
     private List<Site> sites;
