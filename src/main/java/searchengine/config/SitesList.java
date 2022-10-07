1afb63a4ce0f1cdabb3bfb033ea2b0c459c8d57b
Daniil Pilipenko <sortedmap@gmail.com>
2022-10-07 16:45:21 +0300

    Update SitesList.java

--- src/main/java/searchengine/config/SitesList.java
+++ src/main/java/searchengine/config/SitesList.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "index-config")
+@ConfigurationProperties(prefix = "indexing-settings")
 public class SitesList {
     private List<Site> sites;
 }
